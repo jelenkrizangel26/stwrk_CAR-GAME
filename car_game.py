44c4613749f9e8bff2061cff54f5bc8b6b468791
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2022-11-19 14:52:00 +0800

    Adding conditions and options for the game(i use pygame for my car game)

--- car_game.py
+++ car_game.py
@@ -55,4 +55,32 @@ def crash():
     TextSurf, TextRect = text_objects("Oh no! You Crashed (X_X)", largeText)
     gameDisplay.blit(TextSurf, TextRect)
 
-    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.Quit:
+                pygame.quit()
+                quit()
+
+        button("Play Again",150,450,100,50,green,bright_green,game_loop)
+        button("Quit",550,450,100,50,red,bright_red,quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+        if click[0] == 1 and action !=None:
+            action()
+        else:
+            pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+        smallText = pygame.font.SysFont("comicsansms",20)
+        textSurf, textRect =( (x+(w/2)), (y+(h/2)) )
+        gameDisplay.blit(textSurf, textRect)
+
+def quitgame():
+    pygame.quit()
+    quit()
